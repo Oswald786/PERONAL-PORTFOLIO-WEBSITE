e402ec64f57353b4723afa7743766c2e15750b68
Oswald786 <ethanslade63@gmail.com>
2024-10-11 23:00:23 +0100

    NEED TO FIX SERVICE FUNCTION

---
+++ Main.js
@@ -0,0 +1,15 @@
+const WebsiteDevSelected = document.getElementById("WebsiteDevelopement");
+const JavascriptDev = document.getElementById("JavascriptDev");
+const websiteStyling = document.getElementById("websiteStyling")
+
+function showWebDevDescription() {
+    console.log("showing web development service")
+    let ServiceDescription = document.getElementById("ServiceDescription").innerHTML;
+    console.log(ServiceDescription)
+    ServiceDescription = ""
+    console.log(ServiceDescription)
+    let Description = "Custom-crafted website designs tailored to your brand, ensuring a unique and engaging user experience that sets you apart from the competition.";
+    console.log(ServiceDescription)
+    ServiceDescription += Description
+    console.log(ServiceDescription)
+}
