6d476507d52a2d4d9307fd9fafaf34f0a47997cc
Oswald786 <ethanslade63@gmail.com>
2024-10-13 18:41:48 +0100

    Services ection ahs been built awaiting game for the enxt stage

--- Main.js
+++ Main.js
@@ -1,15 +1,48 @@
-const WebsiteDevSelected = document.getElementById("WebsiteDevelopement");
-const JavascriptDev = document.getElementById("JavascriptDev");
-const websiteStyling = document.getElementById("websiteStyling")
+function ShowWebDesignServiceDetails() {
+    let Service1Details = document.getElementById("webSiteDesignDetails");
+    let CurrentDesign = window.getComputedStyle(Service1Details);
 
-function showWebDevDescription() {
-    console.log("showing web development service")
-    let ServiceDescription = document.getElementById("ServiceDescription").innerHTML;
-    console.log(ServiceDescription)
-    ServiceDescription = ""
-    console.log(ServiceDescription)
-    let Description = "Custom-crafted website designs tailored to your brand, ensuring a unique and engaging user experience that sets you apart from the competition.";
-    console.log(ServiceDescription)
-    ServiceDescription += Description
-    console.log(ServiceDescription)
+    let Display = CurrentDesign.getPropertyValue("display")
+    console.log(Display)
+    if (Display === 'none'){
+        Service1Details.style.display = 'block'
+        console.log("display changed to :" + CurrentDesign)
+    }
+    else if (Display === 'block'){
+        Service1Details.style.display  = 'none'
+        console.log("display changed to :" + CurrentDesign)
+    }
+}
+
+function ShowWebOptimization() {
+    let Service1Details = document.getElementById("WebOptimization");
+    let CurrentDesign = window.getComputedStyle(Service1Details);
+
+    let Display = CurrentDesign.getPropertyValue("display")
+    console.log(Display)
+    if (Display === 'none'){
+        Service1Details.style.display = 'block'
+        console.log("display changed to :" + CurrentDesign)
+    }
+    else if (Display === 'block'){
+        Service1Details.style.display  = 'none'
+        console.log("display changed to :" + CurrentDesign)
+    }
+}
+
+function ShowWebSupport(){
+    let Service1Details = document.getElementById("webFixing");
+    let CurrentDesign = window.getComputedStyle(Service1Details);
+
+    let Display = CurrentDesign.getPropertyValue("display")
+    console.log(Display)
+    if (Display === 'none'){
+        Service1Details.style.display = 'block'
+        console.log("display changed to :" + CurrentDesign)
+    }
+    else if (Display === 'block'){
+        Service1Details.style.display  = 'none'
+        console.log("display changed to :" + CurrentDesign)
+    }
+
 }
