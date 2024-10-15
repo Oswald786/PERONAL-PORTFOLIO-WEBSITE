f37375ae3b67945d21d4ad40b3f142beb4a2a9e3
Oswald786 <ethanslade63@gmail.com>
2024-10-15 22:05:26 +0100

    game-has-been-completed-data-validation-error

--- Main.js
+++ Main.js
@@ -47,62 +47,105 @@ function ShowWebSupport(){
 
 }
 
+
+let CorrectNumber = 0;
+let points = 0
+
 function GenerateNumber(){
     UnformattedNumber = Math.random() * 100
     CorrectNumber = Math.floor(UnformattedNumber)
+    return CorrectNumber
+}
+
+
+function GenerateLives(){
+    Lives = 5
+    return Lives
 }
 
 
 function CheckNumber(){
-    UnformattedNumber = Math.random() * 100
-    CorrectNumber = Math.floor(UnformattedNumber)
     console.log(CorrectNumber)
     hint = document.getElementById("hint").innerText
-    answer = document.getElementById("answer").innerHTML
-    Result = document.getElementById("result").innerHTML
-    Lives = 5
+    let answer = parseInt(document.getElementById("answer").value)
+    Result = document.getElementById("result")
 
     lowerBound = CorrectNumber - 5
     UpperBound = CorrectNumber + 5
 
 
     if(Lives >= 1){
-    if(CorrectNumber == answer){
-        Result = ""
-        Result += "Correct the number i am thinking of is " + CorrectNumber
-        ShowResult()
-        console.log("Result is " + Result)
+    if(CorrectNumber === answer){
+        Result.innerHTML = ""
+        Result.innerHTML += "Correct the number i am thinking of is " + CorrectNumber + " you have " + points + " points and " + Lives + " lives left keep going"
+        points += 1
+        GenerateNumber()
+        console.log(points)
+        console.log(CorrectNumber) 
+        console.log(Lives)
     }
     else if(answer > lowerBound && answer < UpperBound){
-        Result = ""
-        Result += "Getting Closer"
+        Result.innerHTML = ""
+        Result.innerHTML += "Getting Closer"
         ShowResult()
-        console.log("Result is " + Result)
-        Lives =- 1
+        console.log("Result is " + Result + " YOU HAVE:" + Lives)
+        Lives -= 1
 
     }
     else if (answer > (CorrectNumber + 5)){
-        Result = ""
-        Result += "Too low"
+        Result.innerHTML = ""
+        Result.innerHTML += "Too HIGH"
         ShowResult()
-        console.log("Result is " + Result)
-        Lives =- 1
+        console.log("Result is " + Result + " YOU HAVE:" + Lives)
+        Lives -= 1
        
     }
     else if (answer < (CorrectNumber - 5)){
-        Result = ""
-        Result += "Too high"
+        Result.innerHTML = ""
+        Result.innerHTML += "Too LOW"
         ShowResult()
-        console.log("Result is " + Result)
-        Lives =- 1
+        console.log("Result is " + Result + " YOU HAVE:" + Lives)
+        Lives -= 1
       
     }
 }
-    if (lives <= 0){
-        Result = ""
-        Result += "You have ran out of lves and will need to restart guess again and it will generate a new number the correct number was " + CorrectNumber
+    if (Lives <= 0){
+        Result.innerHTML = ""
+        Result.innerHTML += "You have ran out of lves and will need to restart guess again and it will generate a new number the correct number was " + CorrectNumber
         Lives = 5
+        DisplayGame()
 }
 }
 
-function ShowResult()
+
+function DisplayGame(){
+    gameInterface = document.getElementById("gameInterface");
+    gameInterfaceState = window.getComputedStyle(gameInterface);
+    DisplayStatus = gameInterfaceState.getPropertyValue("display")
+
+    if (DisplayStatus === "flex"){
+        gameInterface.style.display = "none"
+    }
+    else if (DisplayStatus === "none"){
+        gameInterface.style.display = "flex"
+        GenerateNumber()
+        GenerateLives()
+        console.log(CorrectNumber)
+        console.log(Lives)
+    }
+}
+
+/*function ShowResult(){
+    gameInterface = document.getElementById("gameInterface");
+    gameInterfaceState = window.getComputedStyle(gameInterface);
+    DisplayStatus = gameInterfaceState.getPropertyValue("display")
+
+    Result = CorrectNumber
+    ResultDisplayArea = document.getElementById("result")
+
+
+    if (DisplayStatus === "flex" && Lives <= 0){
+        ResultDisplayArea += Result
+        console.log("RESULT HAS BEEN GIVEN DEPLOYING GAME CLOSOURE")
+    }
+}*/
